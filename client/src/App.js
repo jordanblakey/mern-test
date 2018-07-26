72eb47410b751c99142a36f89c8ba1687387bade
Jordan Blakey <jordan.t.blakey@gmail.com>
2018-07-26 14:34:50 -0500

    Redux working

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-        <h1>Hello</h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     )
   }
 }
